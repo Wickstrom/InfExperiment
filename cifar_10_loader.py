8dd39f38f6f4ab641450dc8090b9c052d4fe5bf4
Kristoffer <wickstrom.kristoffer@gmail.com>
2018-07-16 16:36:29 +0200

    Update

--- cifar_10_loader.py
+++ cifar_10_loader.py
@@ -62,11 +62,6 @@ for epoch in range(2):  # loop over the dataset multiple times
         loss.backward()
         optimizer.step()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+        print(loss)
 
 print('Finished Training')
