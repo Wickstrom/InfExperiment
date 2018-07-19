46e21753099460fc16c46ebef9a2a1afce0d64c0
Kristoffer <wickstrom.kristoffer@gmail.com>
2018-07-19 11:32:05 +0200

    update

--- cifar_10_loader.py
+++ cifar_10_loader.py
@@ -71,10 +71,10 @@ for epoch in range(5):
             print(i)
         if (i % 10) == 0:
             print("MI")
-            model.renyi(inputs)
+            print(model.renyi(inputs))
             for j in range(len(conv_layers)):
 #                print(model.mutual_information(inputs, conv_layers[j].cpu()))
-                model.renyi(conv_layers[j])
+                print(model.renyi(conv_layers[j]))
                 
         
 
