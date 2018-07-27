b40a7d0d2d04455287399e8168452908e48f31c1
Wickstrom <wickstrom.kristoffer@gmail.com>
2018-07-27 14:33:36 +0200

    Update

--- VGG16.py
+++ VGG16.py
@@ -162,8 +162,8 @@ class VGG16(nn.Module):
 #        sigma_x = 5*x.size(0)**(-1/(x.size(1)*x.size(2)))
         x = x.view(x.size(0), -1).data.numpy()
         x = squareform(pdist(x, 'euclidean'))
-        sigma_x = np.mean(np.mean(np.sort(x[:, :10], 1)))
-        x = (1/x.shape[0])*scipy.exp(-x ** 2 / sigma_x ** 2)
+#        sigma_x = np.mean(np.mean(np.sort(x[:, :10], 1)))
+#        x = (1/x.shape[0])*scipy.exp(-x ** 2 / sigma_x ** 2)
         return x
 
     def renyi(self, x):
